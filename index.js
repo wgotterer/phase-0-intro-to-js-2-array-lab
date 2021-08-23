9cc401214f6cf7d2979577c6c55739428949c20c
William Gotterer <william.gotterer@gmail.com>
2021-08-23 09:25:05 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    console.log("Name:", name)
+    cats.push(name);
+    
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+   return [...cats, name];
+}
+
+function prependCat(name) {
+    return[name, ...cats];
+}
+
+function  removeLastCat() {
+   return cats.slice(0, cats.length-1); 
+
+   // return cats.slice(-1); //
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
